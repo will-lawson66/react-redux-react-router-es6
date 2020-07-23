cae82816b14c6c5eccc9e5f795edff6abcedb6db
Bill Lawson <blawson@firstinspires.org>
2020-07-23 08:04:19 -0400

    Comments and config in dev env

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,4 +1,4 @@
-const webpack = require("webpack");
+const webpack = require("webpack"); //CommonJS syntax (vs. ES modules)
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
@@ -7,38 +7,40 @@ process.env.NODE_ENV = "development";
 module.exports = {
   mode: "development",
   target: "web",
-  devtool: "cheap-module-source-map",
+  devtool: "cheap-module-source-map", //see source in browser when debugging
   entry: "./src/index",
   output: {
+    // though webpack serves from memory in development mode, this is still needed
     path: path.resolve(__dirname, "build"),
     publicPath: "/",
-    filename: "bundle.js"
+    filename: "bundle.js",
   },
   devServer: {
+    // configure webpack dev server
     stats: "minimal",
     overlay: true,
     historyApiFallback: true,
     disableHostCheck: true,
     headers: { "Access-Control-Allow-Origin": "*" },
-    https: false
+    https: false,
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
-      favicon: "src/favicon.ico"
-    })
+      favicon: "src/favicon.ico",
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader", "eslint-loader"]
+        use: ["babel-loader", "eslint-loader"],
       },
       {
         test: /(\.css)$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  }
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
