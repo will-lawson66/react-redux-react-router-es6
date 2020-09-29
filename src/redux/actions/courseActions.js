b69411745e3bce4402eb58efb9efa23bd7fb716c
Bill Lawson <blawson@firstinspires.org>
2020-09-29 15:48:43 -0400

    More notes on 9-redux-flow

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,5 +1,6 @@
 import * as types from "./actionTypes";
 
+// this is an action creator
 export function createCourse(course) {
   return { type: types.CREATE_COURSE, course }; //object shorthand syntax: replaces 'course: course'
 } // all actions must have a type property
